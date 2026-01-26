2576b5e7e6fbb24f95bb4a8f3b624e099e56ec02
dimple_1509 <dimpleseervi09@gmail.com>
2026-01-26 18:59:02 +0530

    modified the data and embedding part of code

--- python.py
+++ python.py
@@ -1,4 +1,4 @@
-
+import os
 from langchain_core.documents import Document
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
@@ -7,11 +7,41 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter as rcts  #rc
 from langchain_core.prompts import ChatPromptTemplate
 
 
-def split_into_chunks():
-    file_path = 'info.txt'
+def load_godot_docs(base_path="godot-docs"):
+    all_text = []
 
-    with open(file_path,'r',newline='',encoding='utf-8') as file:
-        txt_data = file.read()
+    EXCLUDED_DIRS = {
+        ".git", ".github", "_static", "locale"
+    }
+
+    EXCLUDED_FILES = {
+        ".gitattributes","Makefile", "robots.txt", ".gitignore", ".editorconfig", ".mailmap",".lycheeignore",".pre-commit-config.yaml",
+        ".readthedocs.yml","404.rst","AUTHORS.md","conf.py","indes.rst","LICENSE.txt","make.bat","Makefile","pyproject.toml","README.md","requirements.txt"
+    }
+
+    for root, dirs, files in os.walk(base_path):
+        # remove unwanted dirs in-place
+        dirs[:] = [d for d in dirs if d not in EXCLUDED_DIRS]
+
+        for file in files:
+            if file in EXCLUDED_FILES:
+                continue
+
+            if file.endswith((".rst", ".md")):
+                file_path = os.path.join(root, file)
+
+                try:
+                    with open(file_path, "r", encoding="utf-8") as f:
+                        all_text.append(f.read())
+                except Exception as e:
+                    print(f"Skipped {file_path}: {e}")
+    print("loaded godot docs")
+    return "\n".join(all_text)
+
+
+def split_into_chunks():
+
+    txt_data = load_godot_docs()
 
     text_splitter = rcts(
         separators = "\n",
@@ -25,7 +55,7 @@ def split_into_chunks():
     chunk.replace("\r\n", " ").replace("\n", " ").replace('"',"").strip() + "..."
     for chunk in chunks
 ]
-
+    print("splitted into chunks")
     return clean_chunks
 
 
@@ -39,17 +69,34 @@ def chunks_into_embeddings(clean_chunks):
     embeddings = HuggingFaceEmbeddings(
         model="sentence-transformers/all-MiniLM-L6-v2"
     )
+    print("initialised embedding")
 
-    vectorstore = FAISS.from_documents(document,embeddings)
+
+    # vectorstore = FAISS.from_documents(document,embeddings)
     #IT IS ACTUALLY THE DATABASE WHICH STORES BOTH THE DOCUMENT CONTENT AND THE VECTOR LIST CORRESPONDING TO IT
+
+
+    BATCH_SIZE = 100
+
+    first_batch = document[:BATCH_SIZE]
+    vectorstore = FAISS.from_documents(first_batch, embeddings)
+    print("vectorstore")
+    
+
+    for i in range(BATCH_SIZE, len(document), BATCH_SIZE):
+        batch = document[i:i + BATCH_SIZE]
+        vectorstore.add_documents(batch)
+        print(f"Embedded {i + len(batch)} / {len(document)}")
+
+    vectorstore.save_local("godot_faiss")
 
     llm = ChatGroq(
         model = "llama-3.1-8b-instant",
         temperature = 0
     )
 
-
-    question = "Is godot free"
+    print("stored chunks with their embeddings")
+    question = "How to use animation tool in godot engine"
 
 # --- similarity search with scores ---
     docs_and_scores = vectorstore.similarity_search_with_score(
@@ -101,6 +148,7 @@ RULES:
     runnable = prompt_runnable | llm
 
 # Execute with invoke()
+    print("calling the model for answer ")
     answer = runnable.invoke({
       "context": context_text,
       "question": question
